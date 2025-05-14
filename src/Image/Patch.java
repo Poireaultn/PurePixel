738af5efb49a87aafeae7d88aa1bb6db32c99377
Charles Mendiburu <charles.mendi1609@gmail.com>
2025-05-14 10:09:12 +0200

    Ajout méthode proj et produitScalaire

--- src/Image/Patch.java
+++ src/Image/Patch.java
@@ -48,4 +48,6 @@ public class Patch {
 
 		return listPatchs;
 	}
+	
+
 }
--- src/Vecteur/Vecteur.java
+++ src/Vecteur/Vecteur.java
@@ -1,34 +1,28 @@
 package Vecteur;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vecteur {
-		private int IDVecteur;
+		
 	    private int taille;
 	    private double[] valeur;
 	    
 	    
 	    //Constructeur par défaut
 	    public Vecteur() {
-	        this.IDVecteur = 0;
 	        this.taille = 0;
 	        this.valeur = new double[0];
 	    }
 	    
 	    //Constructeur avec paramètres
-	    public Vecteur(int IDVecteur, int taille, double[] valeur) {
-	        this.IDVecteur = IDVecteur;
+	    public Vecteur(int taille, double[] valeur) {
+	       
 	        this.taille = taille;
 	        this.valeur = valeur;
 	    }
 	    
-	    //Getter et Setter idVecteur
-	    public int getIDVecteur() {
-	        return IDVecteur;
-	    }
-
-	    public void setIDVecteur(int IDVecteur) {
-	        this.IDVecteur = IDVecteur;
-	    }
-
+	
 	    //Getter et Setter taille
 	    public int getTaille() {
 	        return taille;
@@ -51,7 +45,7 @@ public class Vecteur {
 	    @Override
 	    public String toString() {
 	        StringBuilder sb = new StringBuilder();
-	        sb.append("Vecteur ID: ").append(IDVecteur).append(", Taille: ").append(taille).append(", Valeurs: [");
+	        sb.append("Vecteur ID: ").append(", Taille: ").append(taille).append(", Valeurs: [");
 	        for (int i = 0; i < valeur.length; i++) {
 	            sb.append(valeur[i]);
 	            if (i < valeur.length - 1) {
@@ -61,5 +55,34 @@ public class Vecteur {
 	        sb.append("]");
 	        return sb.toString();
 	    }
+	    
+	    public static double produitScalaire(double[] a, double[] b) {
+	    	double sum=0.0;
+	    	for (int i=0;i<a.length;i++) {
+	    		sum= sum + a[i] * b[i];
+	    	}
+	    	return sum;
+	    }
+	    
+	    //Fait le projeté des vecteurs centrés sur la base orthonormale
+	    public static List<Vecteur> Proj(List<Vecteur> bo, List<Vecteur> listv) {
+	        List<Vecteur> Vcont = new ArrayList<>();
+
+	        for (int k = 0; k < listv.size(); k++) {
+	            Vecteur v = listv.get(k);
+	            double[] alpha = new double[bo.size()];
+
+	            for (int i = 0; i < bo.size(); i++) {
+	                Vecteur u = bo.get(i);
+	                alpha[i] = produitScalaire(u.getValeur(), v.getValeur());
+	            }
+
+	            Vecteur contribution = new Vecteur(alpha.length, alpha);
+	            Vcont.add(contribution);
+	        }
+
+	        return Vcont;
+	    }
+
 
 }
