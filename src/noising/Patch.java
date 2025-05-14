cec3f2b4132dcd9cebd6c2ab2186243907d34c3b
ilyas157 <ilyasbouhafid00@gmail.com>
2025-05-14 12:05:45 +0200

    Update Patch.java

--- src/noising/Patch.java
+++ src/noising/Patch.java
@@ -1,43 +1,26 @@
 package noising;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+
 
 public class Patch {
-	private int  IdPatch;
-	private int Taille;
-	private double[][] Valeur;
 	
+	private int  IdPatch;
+	private int taille;
+	private double[][] valeur;
 	
 	
 	public Patch(int idPatch, int taille, double[][] valeur) {
 		IdPatch = idPatch;
-		Taille = taille;
-		Valeur = valeur;
+		this.taille = taille;
+		this.valeur = valeur;
 	}
-	
-	
-	public int getIdPatch() {
-		return IdPatch;
-	}
-	public void setIdPatch(int idPatch) {
-		IdPatch = idPatch;
-	}
-	public int getTaille() {
-		return Taille;
-	}
-	public void setTaille(int taille) {
-		Taille = taille;
-	}
-	public double[][] getValeur() {
-		return Valeur;
-	}
-	public void setValeur(double[][] valeur) {
-		Valeur = valeur;
-	}
-	public static List<Vector> VectorPatchs(List<Patch> patchs) {
+	public static List<Vecteur> VectorPatchs(List<Patch> patchs) {
 			
-			List<Vector> vectors = new ArrayList<>();
+			List<Vecteur> vectors = new ArrayList<>();
 			
 			for (int i=0;i< patchs.size();i++){
 				
@@ -49,9 +32,77 @@ public class Patch {
 				for (int k=0;k<taille*taille;k++) {
 					colonne[k] = matrice[k/taille][k%taille];
 				}
-				Vector vector = new Vector(id,taille*taille,colonne);
+				Vecteur vector = new Vecteur(id,taille*taille,colonne);
 				vectors.add(vector);
 			}
 			return vectors;
 	}
+	public static List<Patch> ExtractPatchs(Image img, int taille){
+		//Random random = new Random();
+		List<Patch> patchs = new ArrayList<>();
+		List<Integer> shift_possible = new ArrayList<>();
+		
+		int height = img.getHeight();
+		int width = img.getWidth();
+		int indice = 0;
+		for (int i = 1; i <= Math.max(Math.sqrt(height-taille),Math.sqrt(width-taille)); i++) {
+	        if ((height-taille) % i == 0 || (width-taille) % i == 0){
+	            shift_possible.add(i);
+	        }
+	    }
+		int shift = 12;//Collections.max(shift_possible) ;//shift_possible.get(random.nextInt(shift_possible.size()));
+		double[][] pixels = img.getPixels();
+		
+		for (int i=0;i<=height-taille; i+=shift) {
+			for (int j=0;j<=width-taille; j+=shift) {
+				
+				double[][] matrice = new double[taille][taille];
+				for (int k=0;k<taille; k++) {
+					for (int l=0;l<taille; l++) {
+						matrice[l][k] = pixels[j+k][i+l];
+					}
+				}
+				
+				Patch patch = new Patch(indice, taille,matrice);
+				indice +=1;
+				patchs.add(patch);
+				
+			}
+		}		
+		return patchs;
+	}
+	
+	
+	public static void afficherPatch(Patch patch) {
+		System.out.println("le Patch est d'incide : "+ patch.getIdPatch());
+		for (int y = 0; y < patch.getTaille(); y++) {
+			System.out.print("[ ");
+			for (int x = 0; x < patch.getTaille(); x++) {
+				System.out.printf("%.2f ", patch.getValeur()[y][x]);
+			}
+			System.out.println("]");
+		}
+	}
+	
+	
+	
+	public int getIdPatch() {
+		return IdPatch;
+	}
+	public void setIdPatch(int idPatch) {
+		IdPatch = idPatch;
+	}
+	public int getTaille() {
+		return taille;
+	}
+	public void setTaille(int taille) {
+		this.taille = taille;
+	}
+	public double[][] getValeur() {
+		return valeur;
+	}
+	public void setValeur(double[][] valeur) {
+		this.valeur = valeur;
+	}
+	
 }
