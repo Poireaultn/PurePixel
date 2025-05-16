a2f458b618e659ea4202b5ad7c36c1748347c916
Poireaultn <poireault.nathan@gmail.com>
2025-05-16 10:51:14 +0200

    Suppresion fct double

--- src/Image/Patch.java
+++ src/Image/Patch.java
@@ -46,41 +46,6 @@ public class Patch {
 	    }
 	}
 
-	//Permet de construire une list de Patch à partir de l'image de base et leur taille
-	public static List<Patch> ExtractPatchs(Image img, int taille){
-		List<Patch> patchs = new ArrayList<>();
-		List<Integer> shift_possible = new ArrayList<>();
-		
-		int height = img.getHeight();
-		int width = img.getWidth();
-		int indice = 0;
-		for (int i = 1; i <= Math.max(Math.sqrt(height-taille),Math.sqrt(width-taille)); i++) {
-	        if ((height-taille) % i == 0 || (width-taille) % i == 0){
-	            shift_possible.add(i);
-	        }
-	    }
-		int shift = 12;  //soit le max : Collections.max(shift_possible) ; soit aléatoire : //shift_possible.get(random.nextInt(shift_possible.size()));
-		double[][] pixels = img.getPixels();
-		
-		for (int i=0;i<=height-taille; i+=shift) {
-			for (int j=0;j<=width-taille; j+=shift) {
-				
-				double[][] matrice = new double[taille][taille];
-				for (int k=0;k<taille; k++) {
-					for (int l=0;l<taille; l++) {
-						matrice[l][k] = pixels[i+l][j+k];
-					}
-				}
-				
-				Patch patch = new Patch(indice,taille,matrice);
-				indice +=1;
-				patchs.add(patch);
-				
-			}
-		}		
-		return patchs;
-	}
-
 	public static List<Vecteur> VectorPatchs(List<Patch> patchs) {
 			
 			List<Vecteur> vectors = new ArrayList<>();
